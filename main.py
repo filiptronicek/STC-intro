f1a89d4d1f56fd034e9f78474ac037a379944bad
Filip Troníček <filip.tronicek@seznam.cz>
2020-05-12 12:27:34 +0200

    Add font

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
 
-
 #Set the video
 video = VideoFileClip("main.mp4").subclip(50,55)
-print(video.reader.filename)
 
 filenameParts = video.reader.filename.split('.')
 
 filename = ""
 ext = filenameParts[-1]
-print(ext)
 for i,part in enumerate(filenameParts):
     if i != len(filenameParts)-1:
         filename += part
     
+text = "Díl 2 - \n Admin konzole, konfigurace domeny a tvorba identit uživatelů"
+
+
+textParts = text.split("\n")
+for part in textParts:
+    ftSz = 120-(len(part))
+    if(len(part) > 70):
+        ftSz = 120-(0.8*len(text))
 
 # Create the text
-txt_clip = ( TextClip("Jak na Azure AD haha test",fontsize=70,color='white', font='Impact')
+txt_clip = ( TextClip(text,fontsize=ftSz,color='white', font=r'fonts\SEGUISB.TTF')
              .set_position('center')
              .set_duration(5) )
 
