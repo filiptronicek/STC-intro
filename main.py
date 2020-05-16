a68850bfce7490bf2a4246fdd00f638e9ba53dd1
Filip Troníček <filip.tronicek@seznam.cz>
2020-05-16 14:30:57 +0200

    Create server

--- main.py
+++ main.py
@@ -10,30 +10,32 @@ ext = filenameParts[-1]
 for i,part in enumerate(filenameParts):
     if i != len(filenameParts)-1:
         filename += part
-    
-text = "LOREM IPSUM"
 
 w,h = moviesize = video.size
 
-textParts = text.split("\n")
 
 ftSz = 250
-# Create the text
-txt_clip = ( 
-    TextClip(text,fontsize=ftSz,color='white', font='fonts/SEGOEUIB.TTF')
-             .set_position('left')
-             .set_start(0.6)
-             .set_duration(1.4) 
+def generate(text):
+    textParts = text.split("\n")
+
+    # Create the text
+    txt_clip = ( 
+        TextClip(text,fontsize=ftSz,color='white', font='fonts/SEGOEUIB.TTF')
+                .set_position('left')
+                .set_start(0.6)
+                .set_duration(1.4) 
+                )
+
+    txt_mov = txt_clip.set_pos( 
+        lambda t: ( # animate the text
+            min((w*0.03), int(-txt_clip.w-500 + 2.7*w*t)), #X
+            max(1.8*h/6,  #Y
+            int(100*t) #Y
             )
+        ) 
+    )
 
-txt_mov = txt_clip.set_pos( 
-    lambda t: ( # animate the text
-        min((w*0.03), int(-txt_clip.w-500 + 2.7*w*t)), #X
-        max(1.8*h/6,  #Y
-        int(100*t) #Y
-        )
-    ) 
-)
-
-result = CompositeVideoClip([video, txt_mov]) # Overlay text on video
-result.write_videofile(filename+"_edited."+ext,fps=video.reader.fps) # Many options...
+    result = CompositeVideoClip([video, txt_mov]) # Overlay text on video
+    result.write_videofile(filename+"_edited."+ext,fps=video.reader.fps) # Many options...
+    return filename
+#generate(text = "LOREM IPSUM")
---
+++ server.py
@@ -0,0 +1,12 @@
+#!flask/bin/python
+from flask import Flask
+from main import generate
+
+app = Flask(__name__)
+
+@app.route('/create/<string:vid_name>/', methods=["GET"])
+def generateVid(vid_name):
+    return generate(text = vid_name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
