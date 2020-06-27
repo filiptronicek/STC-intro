dc340e33a37a2a6149b02dcc8d160455b9d6ce7e
Filip Troníček <filip.tronicek@seznam.cz>
2020-06-27 21:48:09 +0200

    Add second screen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
+from moviepy.video.fx import fadeout
 import sys, shutil
 
 #Set the video
@@ -18,7 +19,7 @@ outDir = "out/"
 charLimit = 17
 
 ftSz = 250
-def generateVideo(text):
+def generateVideo(text, line2):
     sum = 0
     fnlTxt = ""
     lines = [[],[]]
@@ -40,9 +41,21 @@ def generateVideo(text):
         TextClip(fnlTxt,fontsize=ftSz,color='white', font='fonts/SEGOEUIB.TTF', align="West")
                 .set_position('left')
                 .set_start(0.6)
-                .set_duration(1.8) 
+                .set_duration(1.4) 
                 )
-
+    line2C = ( 
+        TextClip(line2,fontsize=ftSz,color='white', font='fonts/SEGOEUIB.TTF', align="center")
+                .set_position('left')
+                .set_start(2)
+                .set_duration(2) 
+                )
+    line2E = ( 
+    TextClip(line2,fontsize=ftSz,color='white', font='fonts/SEGOEUIB.TTF', align="center")
+            .set_position('left')
+            .set_start(4)
+            .set_duration(1) 
+            .fadeout(1)
+            )
     txt_mov = txt_clip.set_pos( 
         lambda t: ( # animate the text
             min((w*0.1), int(-txt_clip.w-500 + 2.7*w*t)), #X
@@ -52,8 +65,8 @@ def generateVideo(text):
         ) 
     )
     rName = text+"."+ext
-    result = CompositeVideoClip([video, txt_mov]) # Overlay text on video
+    result = CompositeVideoClip([video, txt_mov, line2C, line2E]) # Overlay text on video
     result.write_videofile(rName,fps=video.reader.fps) # Many options...
     shutil.move(rName, "render/"+rName)
     return filename
-generateVideo(text = sys.argv[1])
+generateVideo(text = sys.argv[1], line2 = sys.argv[2])                                                                                                                             
