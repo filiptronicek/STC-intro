f80e2c0e1f3b9116067b76e7906417c6632493dc
Filip Troníček <filip.tronicek@seznam.cz>
2020-06-27 13:31:41 +0200

    Split text to lines

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
 import os, sys, shutil
 
 #Set the video
-video = VideoFileClip("blank.mp4")
+video = VideoFileClip("no_text_4k.mp4")
 
 filenameParts = video.reader.filename.split('.')
 
@@ -15,14 +15,31 @@ for i,part in enumerate(filenameParts):
 w,h = moviesize = video.size
 
 outDir = "out/"
+charLimit = 17
 
 ftSz = 250
 def generate(text):
-    textParts = text.split("\n")
+    sum = 0
+    fnlTxt = ""
+    lines = [[],[]]
+    for i,c in enumerate(text.split()):
+        if sum + len(c) <= charLimit:
+            lines[0] += c
+            if i != 0: fnlTxt += " "
+            fnlTxt += c
+            print(c+": line 1")
+            sum += len(c)
+        elif sum +len(c) <= charLimit * 2:
+            if len(lines[1]) == 0: fnlTxt += "\n"
+            if len(lines[1]) != 0: fnlTxt += " "
+            lines[1] += c
+            fnlTxt += c
+            print(c+": line 2")
+            sum += len(c)
 
     # Create the text
     txt_clip = ( 
-        TextClip(text,fontsize=ftSz,color='white', font='fonts/SEGOEUIB.TTF')
+        TextClip(fnlTxt,fontsize=ftSz,color='white', font='fonts/SEGOEUIB.TTF')
                 .set_position('left')
                 .set_start(0.6)
                 .set_duration(1.4) 
@@ -30,7 +47,7 @@ def generate(text):
 
     txt_mov = txt_clip.set_pos( 
         lambda t: ( # animate the text
-            min((w*0.03), int(-txt_clip.w-500 + 2.7*w*t)), #X
+            min((w*0.1), int(-txt_clip.w-500 + 2.7*w*t)), #X
             max(1.8*h/6,  #Y
             int(100*t) #Y
             )
