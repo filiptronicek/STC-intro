046bd1d997c65fc6d2348c44b1e2e944d8b8b019
Restyled.io <commits@restyled.io>
2020-07-02 09:20:36 +0000

    Restyled by black

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ import os
 # Set the video
 video = VideoFileClip("no_text_4k.mp4")
 
-filenameParts = video.reader.filename.split('.')
+filenameParts = video.reader.filename.split(".")
 
 filename = ""
 ext = filenameParts[-1]
 for i, part in enumerate(filenameParts):
-    if i != len(filenameParts)-1:
+    if i != len(filenameParts) - 1:
         filename += part
 
 w, h = moviesize = video.size
@@ -46,44 +46,40 @@ def generateVideo(text, line2, vId):
     print(fnlTxt)
     # Create the text
     txt_clip = (
-        TextClip(fnlTxt, fontsize=ftSz, color='white',
-                 font=fontPth, align="West")
-        .set_position('left')
+        TextClip(fnlTxt, fontsize=ftSz, color="white", font=fontPth, align="West")
+        .set_position("left")
         .set_start(0.6)
         .set_duration(1.4)
     )
     line2C = (
-        TextClip(line2, fontsize=ftSz, color='white',
-                 font=fontPth, align="center")
-        .set_position('left')
+        TextClip(line2, fontsize=ftSz, color="white", font=fontPth, align="center")
+        .set_position("left")
         .set_start(2)
         .set_duration(2.4)
     )
-    line2C = line2C.set_position(lambda t: (w*0.1, 1.8*h/6))
+    line2C = line2C.set_position(lambda t: (w * 0.1, 1.8 * h / 6))
     line2E = (
-        TextClip(line2, fontsize=ftSz, color='white',
-                 font=fontPth, align="center")
-        .set_position('left')
+        TextClip(line2, fontsize=ftSz, color="white", font=fontPth, align="center")
+        .set_position("left")
         .set_start(4.4)
         .set_duration(0.6)
         .fadeout(0.6)
     )
-    line2E = line2E.set_position(lambda t: (w*0.1, 1.8*h/6))
+    line2E = line2E.set_position(lambda t: (w * 0.1, 1.8 * h / 6))
     txt_mov = txt_clip.set_pos(
         lambda t: (  # animate the text
-            min((w*0.1), int(-txt_clip.w-500 + 2.7*w*t)),  # X
-            max(1.8*h/6,  # Y
-                int(100*t)  # Y
-                )
+            min((w * 0.1), int(-txt_clip.w - 500 + 2.7 * w * t)),  # X
+            max(1.8 * h / 6, int(100 * t)),  # Y  # Y
         )
     )
-    rName = text+"."+ext
+    rName = text + "." + ext
     nName = f"{vId}.{ext}"
     result = CompositeVideoClip(
-        [video, txt_mov, line2C, line2E])  # Overlay text on video
+        [video, txt_mov, line2C, line2E]
+    )  # Overlay text on video
     result.write_videofile(rName, fps=video.reader.fps)  # Many options...
     # Moves the video file to the render directory
-    shutil.move(rName, "render/"+nName)
+    shutil.move(rName, "render/" + nName)
     return filename
 
 
